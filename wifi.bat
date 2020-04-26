ead8916887c71f16bcdc4795b9e45d793f672aef
Harish_kumar_hk <40381981+harish355@users.noreply.github.com>
2020-04-26 23:17:03 +0530

    Update wifi.bat

--- wifi.bat
+++ wifi.bat
@@ -4,4 +4,4 @@ echo welcome BOSS
 echo Start Learning
 pause
 netsh wlan connect name="VIT2.4G"
-start chrome.exe file:///C:/Users/HARISH-NK/Desktop/wifi.html
+start chrome.exe file:///C:/Users/USER-Name/Desktop/wifi.html location of wifi.html
